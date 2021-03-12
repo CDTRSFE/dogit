96b93b44eb534aae36422c5fef7b68d68bbaa743
黄小红 <1246996371@qq.com>
2021-03-12 14:59:20 +0800

    fix config file name in init

--- action/init.js
+++ action/init.js
@@ -5,12 +5,12 @@ const { echo } = require('../lib/helper');
 module.exports = class Init {
     // 读取配置
     readTemplate() {
-        this.template = fs.readFileSync(path.resolve(__dirname, '../template/.dogitrc.json'), 'utf-8');
+        this.template = fs.readFileSync(path.resolve(__dirname, '../template/dogit.config.js'), 'utf-8');
     }
 
     // 写入配置
     writeConfig() {
-        fs.writeFileSync(path.resolve(process.cwd(), '.dogitrc.json'), this.template);
+        fs.writeFileSync(path.resolve(process.cwd(), 'dogit.config.js'), this.template);
     }
 
     start() {
