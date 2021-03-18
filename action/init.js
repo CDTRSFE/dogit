ed1f36cadb1953980190319a14dc5f0b045d4552
黄小红 <1246996371@qq.com>
2021-03-18 11:10:29 +0800

    optimize init logic

--- action/init.js
+++ action/init.js
@@ -3,45 +3,43 @@ const path = require('path');
 const { echo } = require('../lib/helper');
 const prompts = require('prompts');
 module.exports = class Init {
+    constructor() {
+        this.configFilePath = path.resolve(process.cwd(), 'dogit.config.js');
+    }
+
     // 读取配置
     readTemplate() {
         this.template = fs.readFileSync(path.resolve(__dirname, '../template/dogit.config.js'), 'utf-8');
     }
-    // 询问用户是否需要执行配置文件的覆盖
-    async checkIsWrite() {
+
+    async writeConfig() {
         // 判断当前配置文件存不存在
-        const isExist = fs.existsSync(path.resolve(process.cwd(), 'dogit.config.js'));
+        const isExist = fs.existsSync(this.configFilePath);
         // 如果不存在，直接生成配置文件
-        if(!isExist) {
-            fs.writeFileSync(path.resolve(process.cwd(), 'dogit.config.js'), this.template);
-            echo('配置文件生成成功', 'success');
-            return;
-        } 
-        // 配置文件已经存在，判断是否要覆盖
-        const response = await prompts ([
+        if(isExist) {
+            // 配置文件已经存在，判断是否要覆盖
+            const response = await prompts ([
+                {
+                    type: 'toggle',
+                    name: 'isWrite',
+                    message: '配置文件已存在，是否要覆盖？',
+                    initial: false,
+                    active: 'yes',
+                    inactive: 'no'
+                },
+            ],
             {
-                type: 'toggle',
-                name: 'isWrite',
-                message: '是否要覆盖修改的配置文件？',
-                initial: false,
-                active: 'yes',
-                inactive: 'no'
-            },
-        ],
-        {
-            onCancel() {
-                process.exit();
-            }
-        }) 
-        if(response.isWrite) {
-            fs.writeFileSync(path.resolve(process.cwd(), 'dogit.config.js'), this.template);
-        } 
+                onCancel() {
+                    process.exit();
+                }
+            }) 
+            if (!response.isWrite) return
+        }
+        fs.writeFileSync(this.configFilePath, this.template);
         echo('配置文件写入成功','success');
-        
     }
     start() {
         this.readTemplate();
-        this.checkIsWrite();
-
+        this.writeConfig();
     }
 }
